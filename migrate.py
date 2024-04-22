53e41c2830ee246060ec03026a810196542d3f97
benedictclarkson1 <benedictclarkson1@gmail.com>
2024-04-22 23:04:37 +0100

    Added new route submission page

--- migrate.py
+++ migrate.py
@@ -5,11 +5,29 @@ import sqlalchemy as sa
 
 DB_URL = "sqlite:///db.db"
 
-if __name__ == '__main__':
+
+def create_submission_table(engine: sa.engine.Engine):
+    query = """
+    CREATE TABLE IF NOT EXISTS submission(
+        zlaggableName TEXT,
+        cragName TEXT,
+        countryName TEXT,
+        date TEXT,
+        "O-grade" TEXT,
+        difficulty TEXT
+    );
+    """
+    with engine.connect() as db:
+        db.execute(sa.text(query))
+        db.commit()
+
+
+if __name__ == "__main__":
     con = sa.create_engine(DB_URL)
+    create_submission_table(con)
 
     data_path = Path("adam_ondra_ascents.csv")
     df = pd.read_csv(data_path, index_col=0)
-    df['O-grade'] = df['O-grade'].replace("1", "0")
+    df["O-grade"] = df["O-grade"].replace("1", "0")
 
     df.to_sql("ascents", con=con, index=False)
